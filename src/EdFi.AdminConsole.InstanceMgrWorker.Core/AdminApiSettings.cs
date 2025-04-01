21d3036b4d5891b37b0f689378712c3914e1fbe1
David Jimenez <djimenez@growthaccelerationpartners.com>
2025-04-01 13:46:37 -0600

    Changes for on KeyCloak authentication.

--- src/EdFi.AdminConsole.InstanceMgrWorker.Core/AdminApiSettings.cs
+++ src/EdFi.AdminConsole.InstanceMgrWorker.Core/AdminApiSettings.cs
@@ -13,7 +13,10 @@ namespace EdFi.AdminConsole.InstanceMgrWorker.Core
         string AccessTokenUrl { get; set; }
         string Username { get; set; }
         string ClientId { get; set; }
+        string ClientSecret { get; set; }
         string Password { get; set; }
+        string GrantType { get; set; }
+        string Scope { get; set; }
     }
 
     public sealed class AdminApiSettings : IAdminApiSettings
@@ -24,6 +27,9 @@ namespace EdFi.AdminConsole.InstanceMgrWorker.Core
         public string AccessTokenUrl { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
         public string ClientId { get; set; } = string.Empty;
+        public string ClientSecret { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public string GrantType { get; set; } = string.Empty;
+        public string Scope { get; set; } = string.Empty;
     }
 }
